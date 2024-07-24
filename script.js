45abc55f6991901df2c1073baf98469f8a4f608c
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-07-24 23:40:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,216 @@
+const baseURL = "https://v6.exchangerate-api.com/v6/6f0c226b77014ad0ac8af364/latest/";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const button = document.querySelector("form button");
+const fromCurrency = document.querySelector(".from select");
+const toCurrency = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+for (let select of dropdowns) {
+    for (let currencyCode in countryList) {
+        let newOptions = document.createElement("option");
+        newOptions.innerText = currencyCode;
+        newOptions.value = currencyCode;
+        if (select.name === "from" && currencyCode === "USD") {
+            newOptions.selected = "selected";
+        } else if (select.name === "to" && currencyCode === "PKR") {
+            newOptions.selected = "selected";
+        }
+        select.append(newOptions);
+    }
+    select.addEventListener("change", (event) => {
+        updateFlag(event.target);
+    })
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector('img');
+    img.src = newSrc;
+}
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amountValue = amount.value;
+    if (amount.value === "" || amountValue < 1) {
+        amountValue = 1;
+        amount.value = "1";
+    }
+    const URL = `${baseURL}${fromCurrency.value}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let consversionRate = data.conversion_rates[toCurrency.value];
+    let finalAmount = (consversionRate * amountValue).toFixed(2);
+    msg.innerHTML = `${amountValue}${fromCurrency.value} = ${finalAmount}${toCurrency.value}`;
+}
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
